0ff2802b0a1abc545e563f74501c42360afc02ff
ben <ben76020567@gmail.com>
2025-04-17 02:26:29 +0800

    update

--- src/lib/mongoDB/mongo-builder.ts
+++ src/lib/mongoDB/mongo-builder.ts
@@ -1,3 +1,90 @@
+import { Model } from "mongoose";
+
 export class MongoBuilder {
-  private pipeline: any[] = [];
+  private pipeline: any = [];
+  private model: Model<any> | null = null;
+
+  constructor(model?: Model<any>) {
+    if (model) {
+      this.model = model;
+    }
+  }
+
+  From(model: Model<any>) {
+    this.model = model;
+    return this;
+  }
+
+  Where(predict: any) {
+    this.pipeline.push({ $match: predict });
+    return this;
+  }
+
+  Lookup(from: string, localField: string, foreignField: string, as: string) {
+    this.pipeline.push({ $lookup: { from, localField, foreignField, as } });
+    return this;
+  }
+
+  LeftJoin(from: string, localField: string, foreignField: string, as: string) {
+    this.pipeline.push({ $lookup: { from, localField, foreignField, as } });
+    this.pipeline.push({
+      $unwind: { path: `$${as}`, preserveNullAndEmptyArrays: true },
+    });
+    return this;
+  }
+
+  InnerJoin(
+    from: string,
+    localField: string,
+    foreignField: string,
+    as: string,
+  ) {
+    this.pipeline.push({ $lookup: { from, localField, foreignField, as } });
+    this.pipeline.push({
+      $unwind: { path: `$${as}`, preserveNullAndEmptyArrays: false },
+    });
+    return this;
+  }
+
+  Project(project: any) {
+    this.pipeline.push({ $project: project });
+    return this;
+  }
+
+  async Query() {
+    if (!this.model) {
+      throw new Error("Model not set");
+    }
+    return await this.model.aggregate(this.pipeline);
+  }
+
+  Pipe(pipeline: any) {
+    this.pipeline.push(pipeline);
+    return this;
+  }
+
+  GroupBy(group: any) {
+    this.pipeline.push({ $group: group });
+    return this;
+  }
+
+  Sort(sort: any) {
+    this.pipeline.push({ $sort: sort });
+    return this;
+  }
+
+  Limit(limit: any) {
+    this.pipeline.push({ $limit: limit });
+    return this;
+  }
+
+  Skip(skip: any) {
+    this.pipeline.push({ $skip: skip });
+    return this;
+  }
+
+  Unwind(prop: any) {
+    this.pipeline.push({ $unwind: prop });
+    return this;
+  }
 }
