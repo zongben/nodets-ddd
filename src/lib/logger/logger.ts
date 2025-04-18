2c431e2fae57322c41475965ad0beca6cff0217a
Ben <ben76020567@gmail.com>
2025-04-18 17:44:32 +0800

    update

--- src/lib/logger/logger.ts
+++ src/lib/logger/logger.ts
@@ -1,11 +1,25 @@
 import winston from "winston";
 import "dotenv/config";
 
-export class Logger {
+class Logger {
   private logger: winston.Logger;
 
   constructor() {
+    this.logger = winston.createLogger({
+      format: winston.format.combine(
+        winston.format.timestamp({
+          format: "YYYY-MM-DD HH:mm:ss",
+        }),
+        winston.format.printf(({ timestamp, level, message }) => {
+          return `${timestamp} ${level}: ${message}`;
+        }),
+      ),
+    });
+  }
+
+  private setOptions() {
     const env = process.env.NODE_ENV!;
+    const level = env === "dev" ? "debug" : "info";
     const today = new Date()
       .toLocaleDateString("zh-TW", {
         year: "numeric",
@@ -19,33 +33,30 @@ export class Logger {
         ? new winston.transports.Console()
         : new winston.transports.File({ filename: `./log/${today}.log` });
 
-    this.logger = winston.createLogger({
-      level: env === "dev" ? "debug" : "info",
-      format: winston.format.combine(
-        winston.format.timestamp({
-          format: "YYYY-MM-DD HH:mm:ss",
-        }),
-        winston.format.printf(({ timestamp, level, message }) => {
-          return `${timestamp} ${level}: ${message}`;
-        }),
-      ),
-      transports: [transport],
-    });
+    this.logger.level = level;
+    this.logger.clear();
+    this.logger.add(transport)
   }
 
   error(err: Error) {
+    this.setOptions();
     this.logger.error(err.stack || err.message);
   }
 
   warn(message: string) {
+    this.setOptions();
     this.logger.warn(message);
   }
 
   info(message: string) {
+    this.setOptions();
     this.logger.info(message);
   }
 
   debug(message: string) {
+    this.setOptions();
     this.logger.debug(message);
   }
 }
+
+export const logger = new Logger();
