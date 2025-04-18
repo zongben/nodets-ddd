38fbb2f71bd52cc04bb76d6da2d706d1ec1ea3a1
Ben <ben76020567@gmail.com>
2025-04-18 17:55:22 +0800

    update

--- src/lib/middleware/exception.middleware.ts
+++ src/lib/middleware/exception.middleware.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import type { Request, Response } from "express";
 import { ErrorResponse } from "../controller/error-response";
+import { logger } from "../logger/logger";
 
 export function exceptionMiddleware(
   err: Error,
@@ -8,8 +9,7 @@ export function exceptionMiddleware(
   res: Response,
   _next: any,
 ) {
-  console.error(err);
-
+  logger.error(err)
   res
     .status(500)
     .send(new ErrorResponse("INTERNAL_SERVER_ERROR", "Internal server error"));
