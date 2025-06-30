37cf7395846204d4b885cafc73ba2d8712457c1a
ben <ben76020567@gmail.com>
2025-06-30 16:16:45 +0800

    refac: not found middleware

--- src/lib/middleware/notfound.middleware.ts
+++ src/lib/middleware/notfound.middleware.ts
@@ -1,8 +1,9 @@
 import { ILogger } from "../bootstrap/interfaces/logger.interface";
 import { ErrorResponse } from "../controller/error-response";
+import type { Request, Response } from "express";
 
 export function notFoundMiddleware(logger: ILogger) {
-  return (req: any, res: any) => {
+  return (req: Request, res: Response) => {
     logger.warn(`Not found: ${req.method} ${req.originalUrl}`);
     res.status(404).json(new ErrorResponse("NOT_FOUND", "Not found"));
   };
