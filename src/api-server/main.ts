bcb09d576c2de5517eed92e37c0dbca1f80990a8
ben <ben76020567@gmail.com>
2025-07-07 14:45:50 +0800

    rm: comments

--- src/api-server/main.ts
+++ src/api-server/main.ts
@@ -4,9 +4,6 @@ import { jwtValidHandler } from "../lib/controller/jwt-valid-handler";
 import path from "node:path";
 import { MediatorModule } from "../lib/mediator/mediator.module";
 import { notFoundMiddleware } from "../lib/middleware/notfound.middleware";
-// import { Mongo } from "../lib/mongoDB/mongo";
-// import { models } from "./infra/schemas";
-// import { MongoModule } from "../lib/mongoDB/mongo.module";
 import { exceptionMiddleware } from "../lib/middleware/exception.middleware";
 import { map } from "./application/handler.map";
 import { JwTokenHelperModule } from "../lib/jwToken/jwtoken.module";
@@ -30,10 +27,6 @@ const app = App.createBuilder((opt) => {
   };
 });
 
-// const mongo = Mongo.create(app.env.get("MONGO_URL")).addModels(models);
-// mongo.tryConnect(app.logger);
-// mongo.trySyncIndexs(app.logger);
-
 app.loadModules(
   new MediatorModule(app.serviceContainer, map),
   new JwTokenHelperModule(
@@ -52,7 +45,6 @@ app.loadModules(
       }),
     ),
   ),
-  // new MongoModule(mongo),
 );
 app.useCors({
   origin: app.env.get("CORS_ORIGIN"),
