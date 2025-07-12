7e9e2f5a02fb25b7f39b9f27967975e4daf5b482
ben <ben76020567@gmail.com>
2025-07-12 18:21:39 +0800

    ft: validator options

--- src/lib/controller/validater.ts
+++ src/lib/controller/validater.ts
@@ -1,15 +1,31 @@
 import { Request, Response, NextFunction } from "express";
-import { validationResult, ValidationChain } from "express-validator";
+import {
+  validationResult,
+  ValidationChain,
+  ValidationError,
+} from "express-validator";
 import type { ExpressMiddleware } from "./decorator/controller.decorator";
 
-export const validate = (chains: ValidationChain[]): ExpressMiddleware => {
+export const validate = <T = any>(
+  chains: ValidationChain[],
+  options?: {
+    status?: number;
+    handler?: (errors: ValidationError[]) => T;
+  },
+): ExpressMiddleware => {
   return async (req: Request, res: Response, next: NextFunction) => {
     for (const chain of chains) {
       await chain.run(req);
     }
     const errs = validationResult(req);
     if (!errs.isEmpty()) {
-      return res.status(400).json(errs.array().map((x) => x.msg));
+      return res
+        .status(options?.status ?? 400)
+        .json(
+          options?.handler
+            ? options.handler(errs.array())
+            : errs.array().map((x) => x.msg),
+        );
     }
     next();
   };
